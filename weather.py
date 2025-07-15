c7e8c1d8c96cdb020ca31e732d9c04abcfc935fd
rlaxogudp <140497435+rlaxogudp@users.noreply.github.com>
2025-07-15 13:26:50 +0900

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,8 +3,8 @@ import csv # csv로 저장
 import os # 폴더 생성
 from datetime import datetime # 시간 변환
 city = "seoul"
-API_KEY_W = os.getenv("API_KEY_W")
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric"
 response = requests.get(url)
 result = response.json()
 temp = result["main"]["temp"] # 현재 기온
